14483f432f7f5a4b860c252945f1e00a5f51e1e3
adiluzio <adiluzio82@gmail.com>
2022-01-19 22:39:27 +0100

    Change dicci_qualcosa function

--- conte_bot.py
+++ conte_bot.py
@@ -21,45 +21,29 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Nessuno può aiutarti, nemmeno io!')
 
 def dicci_qualcosa(update: Update, context: CallbackContext) -> None:
-    """Send a message of your choice."""
-    if update.message.text == '1':
-        context.bot.send_message(message_id = update.message.message_id,
-                             chat_id = update.message.chat_id,
-                             text = 'Old')
-    elif update.message.text == '2':
-        context.bot.send_message(message_id = update.message.message_id,
-                             chat_id = update.message.chat_id,
-                             text = u'\U0001F611')
-    elif update.message.text == '3':
-        context.bot.send_message(message_id = update.message.message_id,
-                             chat_id = update.message.chat_id,
-                             text = u'\U0001F644')
-    elif update.message.text == '4':
-        context.bot.send_message(message_id = update.message.message_id,
-                             chat_id = update.message.chat_id,
-                             text = 'Moggi era innocente! \U0001F624')
-    elif update.message.text == '5':
-        context.bot.send_message(message_id = update.message.message_id,
-                             chat_id = update.message.chat_id,
-                             text = 'L\'ho fatto/visto/sentito/scoperto/mangiato/visitato prima io!')
-    elif update.message.text == '6':
-        context.bot.send_message(message_id = update.message.message_id,
-                             chat_id = update.message.chat_id,
-                             text = 'Eh vabbè...')
-    else:
-        context.bot.send_message(message_id = update.message.message_id,
-                             chat_id = update.message.chat_id,
-                             text = 'Non ci penso proprio! \U0001F624')
+    """Send a random message."""
+    sentences = [
+        'Old',
+        u'\U0001F611',
+        u'\U0001F644',
+        'L\'ho fatto/visto/sentito/scoperto/mangiato/visitato prima io!',
+        'Eh vabbè...',
+        'Non ci penso proprio! \U0001F624',
+    ]
+    context.bot.send_message(message_id = update.message.message_id,
+                            chat_id = update.message.chat_id,
+                            text = random.choice(sentences))
 
 
 def lamentati(update: Update, context: CallbackContext) -> None:
-    """Send a random message."""
+    """Send a random complaint."""
     sentences = [
         'Mi rifiuto di parlare di calcio con voi che non ne sapete un ca**o! \U0001F624',
         'Mi rifiuto di parlare con gente che non sa cosa significhi "pleonastico" \U0001F624',
         'La pizza napoletana mi ha stufato... a Milano esiste solo quella... \U0001F624',
         'Non ho mai detto che la De Filippi sia bella, è una legenda metropolitana! \U0001F624',
-        'Quando lavoravo per il tizio di FI mi avete isolato tutti... \u2639\uFE0F'
+        'Quando lavoravo per il tizio di FI mi avete isolato tutti... \u2639\uFE0F',
+        'Moggi era innocente! \U0001F624',
     ]
     context.bot.send_message(message_id = update.message.message_id,
                             chat_id = update.message.chat_id,
